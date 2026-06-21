d10d5997275b4b408affba6acfce46c488488058
Viet Anh Tran <42197149+vietanhrs@users.noreply.github.com>
2026-06-21 13:16:19 +0700

    Fix NeoForge event registration

--- src/neoforge/java/com/autofarm/neoforge/AutoFarmNeoForgeMod.java
+++ src/neoforge/java/com/autofarm/neoforge/AutoFarmNeoForgeMod.java
@@ -3,51 +3,41 @@ package com.autofarm.neoforge;
 import com.autofarm.config.AutoFarmConfig;
 import com.autofarm.handler.AutoAttackHandler;
 import com.autofarm.handler.AutoFishingHandler;
-import net.neoforged.fml.IExtensionPoint;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.neoforge.client.event.ClientChatEvent;
-import net.neoforged.neoforge.event.TickEvent;
-
-import java.util.function.Predicate;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(AutoFarmNeoForgeMod.MOD_ID)
 public class AutoFarmNeoForgeMod {
 
     public static final String MOD_ID = "autofarm";
 
-    public AutoFarmNeoForgeMod(FMLJavaModLoadingContext context) {
+    public AutoFarmNeoForgeMod(ModContainer modContainer) {
         AutoFarmConfig.configure(
                 NeoForgeAutoFarmConfig.ATTACK_INTERVAL_SECONDS::get,
                 NeoForgeAutoFarmConfig.CRITICAL_FOOD_LEVEL::get
         );
 
-        context.registerConfig(ModConfig.Type.CLIENT, NeoForgeAutoFarmConfig.SPEC);
-        context.registerDisplayTest(IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);
-
-        FMLClientSetupEvent.getBus(context.getModBusGroup()).addListener(AutoFarmNeoForgeMod::onClientSetup);
+        modContainer.registerConfig(ModConfig.Type.CLIENT, NeoForgeAutoFarmConfig.SPEC);
+        NeoForge.EVENT_BUS.addListener((ClientTickEvent.Post event) -> AutoAttackHandler.onClientTick());
+        NeoForge.EVENT_BUS.addListener((ClientTickEvent.Post event) -> AutoFishingHandler.onClientTick());
+        NeoForge.EVENT_BUS.addListener(AutoFarmNeoForgeMod::onClientChat);
     }
 
-    private static void onClientSetup(FMLClientSetupEvent event) {
-        TickEvent.ClientTickEvent.Post.BUS.addListener(event$ -> AutoAttackHandler.onClientTick());
-        TickEvent.ClientTickEvent.Post.BUS.addListener(event$ -> AutoFishingHandler.onClientTick());
-        ClientChatEvent.BUS.addListener((Predicate<ClientChatEvent>) AutoFarmNeoForgeMod::onClientChat);
-    }
-
-    private static boolean onClientChat(ClientChatEvent event) {
+    private static void onClientChat(ClientChatEvent event) {
         String msg = event.getMessage().trim().toLowerCase();
 
         if (msg.startsWith("!attack")) {
             AutoAttackHandler.handleCommand(msg);
-            return true;
+            event.setCanceled(true);
+            return;
         }
         if (msg.startsWith("!fish")) {
             AutoFishingHandler.handleCommand(msg);
-            return true;
+            event.setCanceled(true);
         }
-
-        return false;
     }
 }
